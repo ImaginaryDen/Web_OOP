32f34c67a6de08192db6c51d325d6053cb11654f
imaginaryDen <denis.marakov@mail.ru>
2021-04-13 21:33:10 +0300

    UPD

--- WebApplication1/Models/User_Data.cs
+++ WebApplication1/Models/User_Data.cs
@@ -13,6 +13,5 @@ namespace WebApplication1.Models
 		public string Email { get; set; }
 		public string Password { get; set; }
 		public Diary Diary_data { get; set; }
-		public string test { get; set; }
 	}
 }
