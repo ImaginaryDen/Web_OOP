c8db554c5421d4ced9bdf8f7804a500e4cfcd041
Василий Шелекасов <Vas.Shell2Win@yandex.ru>
2021-05-13 16:03:39 +0300

    Реально пофикшено

--- ToDoBook/Storage/Entity/TimerEntry.cs
+++ ToDoBook/Storage/Entity/TimerEntry.cs
@@ -8,7 +8,12 @@ namespace ToDoBook.Storage.Entity
     public class TimerEntry : Entry
     {
         public DateTime EndTime { get; set; }
-        public TimeSpan TimeLeft { get => EndTime - DateTime.Now; }
+        public TimeSpan TimeLeft { get {
+                TimeSpan value = EndTime - DateTime.Now;
+                if (value < TimeSpan.Zero) value = TimeSpan.Zero;
+                return value;
+                }
+            }
 
         public TimerEntry()
         {
